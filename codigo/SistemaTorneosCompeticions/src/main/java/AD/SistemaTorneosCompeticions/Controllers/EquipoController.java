d7f4cb7ef6f7bcf9d1b51499dccea7c28e4f2f92
Jorge Sobrino <159003886+Jsobrino98@users.noreply.github.com>
2025-02-10 09:38:22 +0100

    EquipoController
    Metodos CRUD EquipoController

---
+++ codigo/SistemaTorneosCompeticions/src/main/java/AD/SistemaTorneosCompeticions/Controllers/EquipoController.java
@@ -0,0 +1,57 @@
+package AD.SistemaTorneosCompeticions.Controllers;
+
+import AD.SistemaTorneosCompeticions.Models.Equipo;
+import AD.SistemaTorneosCompeticions.Services.EquipoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/equipo")
+public class EquipoController {
+
+    @Autowired
+    private EquipoService equipoService;
+
+    // Obtener todos los equipos
+    @GetMapping
+    public List<Equipo> obtenerEquipo() {
+        return equipoService.obtenerTodos();
+    }
+
+    // Obtener equipo por ID
+    @GetMapping("/{id}")
+    public Equipo obtenerEquipo(@PathVariable Long id) {
+        return equipoService.obtener(id);
+    }
+
+    // Guardar un nuevo Equipo
+    @PostMapping
+    public Equipo guardarEquipo(@RequestBody Equipo equipo) {
+        return equipoService.guardar(equipo);
+    }
+
+    // Actualizar un equipo existente
+    @PutMapping("/{id}")
+    public ResponseEntity<?> actualizar(@PathVariable Long id, @RequestBody Equipo equipo) {
+        if (id.equals(equipo.getId())) {
+            equipoService.guardar(equipo);
+            return ResponseEntity.accepted().body("El equipo se ha actualizado correctamente");
+        } else {
+            return ResponseEntity.badRequest().body("El equipo no se ha podido actualizar, los id no coinciden o no existe");
+        }
+    }
+
+    // Eliminar un equipo
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> borrarEquipo(@PathVariable Long id) {
+        equipoService.eliminar(id);
+        return ResponseEntity.ok().body("El equipo ha sido eliminado correctamente");
+
+    }
+
+
+
+}
---
+++ documentacion/BBDD Premier League.sql
@@ -0,0 +1,170 @@
+-- PREMIER LEAGUE --
+-- {
+--   "equipos": [
+--     {
+--       "id": 1,
+--       "nombre": "Arsenal",
+--       "ciudad": "London",
+--       "torneo_id": 1,
+--       "escudoURL": "https://cdn.footystats.org/img/teams/arsenal-fc.png"
+--     },
+--     {
+--       "id": 2,
+--       "nombre": "Manchester City",
+--       "ciudad": "Manchester",
+--       "torneo_id": 1,
+--       "escudoURL": "https://cdn.footystats.org/img/teams/manchester-city-fc.png"
+--     },
+--     {
+--       "id": 3,
+--       "nombre": "Manchester United",
+--       "ciudad": "Manchester",
+--       "torneo_id": 1,
+--       "escudoURL": "https://cdn.footystats.org/img/teams/manchester-united-fc.png"
+--     },
+--     {
+--       "id": 4,
+--       "nombre": "Chelsea",
+--       "ciudad": "London",
+--       "torneo_id": 1,
+--       "escudoURL": "https://cdn.footystats.org/img/teams/chelsea-fc.png"
+--     },
+--     {
+--       "id": 5,
+--       "nombre": "Liverpool",
+--       "ciudad": "Liverpool",
+--       "torneo_id": 1,
+--       "escudoURL": "https://cdn.footystats.org/img/teams/liverpool-fc.png"
+--     },
+--     {
+--       "id": 6,
+--       "nombre": "Tottenham Hotspur",
+--       "ciudad": "London",
+--       "torneo_id": 1,
+--       "escudoURL": "https://cdn.footystats.org/img/teams/tottenham-hotspur-fc.png"
+--     },
+--     {
+--       "id": 7,
+--       "nombre": "Newcastle United",
+--       "ciudad": "Newcastle",
+--       "torneo_id": 1,
+--       "escudoURL": "https://cdn.footystats.org/img/teams/newcastle-united-fc.png"
+--     },
+--     {
+--       "id": 8,
+--       "nombre": "Aston Villa",
+--       "ciudad": "Birmingham",
+--       "torneo_id": 1,
+--       "escudoURL": "https://cdn.footystats.org/img/teams/aston-villa-fc.png"
+--     },
+--     {
+--       "id": 9,
+--       "nombre": "West Ham United",
+--       "ciudad": "London",
+--       "torneo_id": 1,
+--       "escudoURL": "https://cdn.footystats.org/img/teams/west-ham-united-fc.png"
+--     },
+--     {
+--       "id": 10,
+--       "nombre": "Brighton & Hove Albion",
+--       "ciudad": "Brighton",
+--       "torneo_id": 1,
+--       "escudoURL": "https://cdn.footystats.org/img/teams/brighton-and-hove-albion-fc.png"
+--     },
+--     {
+--       "id": 11,
+--       "nombre": "Brentford",
+--       "ciudad": "London",
+--       "torneo_id": 1,
+--       "escudoURL": "https://cdn.footystats.org/img/teams/brentford-fc.png"
+--     },
+--     {
+--       "id": 12,
+--       "nombre": "Crystal Palace",
+--       "ciudad": "London",
+--       "torneo_id": 1,
+--       "escudoURL": "https://cdn.footystats.org/img/teams/crystal-palace-fc.png"
+--     },
+--     {
+--       "id": 13,
+--       "nombre": "Fulham",
+--       "ciudad": "London",
+--       "torneo_id": 1,
+--       "escudoURL": "https://cdn.footystats.org/img/teams/fulham-fc.png"
+--     },
+--     {
+--       "id": 14,
+--       "nombre": "Wolverhampton Wanderers",
+--       "ciudad": "Wolverhampton",
+--       "torneo_id": 1,
+--       "escudoURL": "https://cdn.footystats.org/img/teams/wolverhampton-wanderers-fc.png"
+--     },
+--     {
+--       "id": 15,
+--       "nombre": "Nottingham Forest",
+--       "ciudad": "Nottingham",
+--       "torneo_id": 1,
+--       "escudoURL": "https://cdn.footystats.org/img/teams/nottingham-forest-fc.png"
+--     },
+--     {
+--       "id": 16,
+--       "nombre": "Everton",
+--       "ciudad": "Liverpool",
+--       "torneo_id": 1,
+--       "escudoURL": "https://cdn.footystats.org/img/teams/everton-fc.png"
+--     },
+--     {
+--       "id": 17,
+--       "nombre": "Burnley",
+--       "ciudad": "Burnley",
+--       "torneo_id": 1,
+--       "escudoURL": "https://cdn.footystats.org/img/teams/burnley-fc.png"
+--     },
+--     {
+--       "id": 18,
+--       "nombre": "Sheffield United",
+--       "ciudad": "Sheffield",
+--       "torneo_id": 1,
+--       "escudoURL": "https://cdn.footystats.org/img/teams/sheffield-united-fc.png"
+--     },
+--     {
+--       "id": 19,
+--       "nombre": "Luton Town",
+--       "ciudad": "Luton",
+--       "torneo_id": 1,
+--       "escudoURL": "https://cdn.footystats.org/img/teams/luton-town-fc.png"
+--     },
+--     {
+--       "id": 20,
+--       "nombre": "Bournemouth",
+--       "ciudad": "Bournemouth",
+--       "torneo_id": 1,
+--       "escudoURL": "https://cdn.footystats.org/img/teams/afc-bournemouth.png"
+--     }
+--   ]
+-- }
+
+INSERT INTO equipos (nombre, ciudad, torneo_id, escudoURL) VALUES
+('Arsenal', 'London', 1, 'https://cdn.footystats.org/img/teams/arsenal-fc.png'),
+('Manchester City', 'Manchester', 1, 'https://cdn.footystats.org/img/teams/manchester-city-fc.png'),
+('Manchester United', 'Manchester', 1, 'https://cdn.footystats.org/img/teams/manchester-united-fc.png'),
+('Chelsea', 'London', 1, 'https://cdn.footystats.org/img/teams/chelsea-fc.png'),
+('Liverpool', 'Liverpool', 1, 'https://cdn.footystats.org/img/teams/liverpool-fc.png'),
+('Tottenham Hotspur', 'London', 1, 'https://cdn.footystats.org/img/teams/tottenham-hotspur-fc.png'),
+('Newcastle United', 'Newcastle', 1, 'https://cdn.footystats.org/img/teams/newcastle-united-fc.png'),
+('Aston Villa', 'Birmingham', 1, 'https://cdn.footystats.org/img/teams/aston-villa-fc.png'),
+('West Ham United', 'London', 1, 'https://cdn.footystats.org/img/teams/west-ham-united-fc.png'),
+('Brighton & Hove Albion', 'Brighton', 1, 'https://cdn.footystats.org/img/teams/brighton-and-hove-albion-fc.png'),
+('Brentford', 'London', 1, 'https://cdn.footystats.org/img/teams/brentford-fc.png'),
+('Crystal Palace', 'London', 1, 'https://cdn.footystats.org/img/teams/crystal-palace-fc.png'),
+('Fulham', 'London', 1, 'https://cdn.footystats.org/img/teams/fulham-fc.png'),
+('Wolverhampton Wanderers', 'Wolverhampton', 1, 'https://cdn.footystats.org/img/teams/wolverhampton-wanderers-fc.png'),
+('Nottingham Forest', 'Nottingham', 1, 'https://cdn.footystats.org/img/teams/nottingham-forest-fc.png'),
+('Everton', 'Liverpool', 1, 'https://cdn.footystats.org/img/teams/everton-fc.png'),
+('Burnley', 'Burnley', 1, 'https://cdn.footystats.org/img/teams/burnley-fc.png'),
+('Sheffield United', 'Sheffield', 1, 'https://cdn.footystats.org/img/teams/sheffield-united-fc.png'),
+('Luton Town', 'Luton', 1, 'https://cdn.footystats.org/img/teams/luton-town-fc.png'),
+('Bournemouth', 'Bournemouth', 1, 'https://cdn.footystats.org/img/teams/afc-bournemouth.png');
+
+
+
