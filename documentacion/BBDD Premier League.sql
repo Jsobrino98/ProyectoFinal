89165f2e28b3e99ea3ec067b55bd55d06b81de33
Jorge Sobrino <159003886+Jsobrino98@users.noreply.github.com>
2025-02-10 10:39:20 +0100

    Escudos LaLiga

--- documentacion/BBDD Premier League.sql
+++ documentacion/BBDD Premier League.sql
@@ -145,26 +145,33 @@
 -- }
 
 INSERT INTO equipos (nombre, ciudad, torneo_id, escudoURL) VALUES
-('Arsenal', 'London', 1, 'https://cdn.footystats.org/img/teams/arsenal-fc.png'),
-('Manchester City', 'Manchester', 1, 'https://cdn.footystats.org/img/teams/manchester-city-fc.png'),
-('Manchester United', 'Manchester', 1, 'https://cdn.footystats.org/img/teams/manchester-united-fc.png'),
-('Chelsea', 'London', 1, 'https://cdn.footystats.org/img/teams/chelsea-fc.png'),
-('Liverpool', 'Liverpool', 1, 'https://cdn.footystats.org/img/teams/liverpool-fc.png'),
-('Tottenham Hotspur', 'London', 1, 'https://cdn.footystats.org/img/teams/tottenham-hotspur-fc.png'),
-('Newcastle United', 'Newcastle', 1, 'https://cdn.footystats.org/img/teams/newcastle-united-fc.png'),
-('Aston Villa', 'Birmingham', 1, 'https://cdn.footystats.org/img/teams/aston-villa-fc.png'),
-('West Ham United', 'London', 1, 'https://cdn.footystats.org/img/teams/west-ham-united-fc.png'),
-('Brighton & Hove Albion', 'Brighton', 1, 'https://cdn.footystats.org/img/teams/brighton-and-hove-albion-fc.png'),
-('Brentford', 'London', 1, 'https://cdn.footystats.org/img/teams/brentford-fc.png'),
-('Crystal Palace', 'London', 1, 'https://cdn.footystats.org/img/teams/crystal-palace-fc.png'),
-('Fulham', 'London', 1, 'https://cdn.footystats.org/img/teams/fulham-fc.png'),
-('Wolverhampton Wanderers', 'Wolverhampton', 1, 'https://cdn.footystats.org/img/teams/wolverhampton-wanderers-fc.png'),
-('Nottingham Forest', 'Nottingham', 1, 'https://cdn.footystats.org/img/teams/nottingham-forest-fc.png'),
-('Everton', 'Liverpool', 1, 'https://cdn.footystats.org/img/teams/everton-fc.png'),
-('Burnley', 'Burnley', 1, 'https://cdn.footystats.org/img/teams/burnley-fc.png'),
-('Sheffield United', 'Sheffield', 1, 'https://cdn.footystats.org/img/teams/sheffield-united-fc.png'),
-('Luton Town', 'Luton', 1, 'https://cdn.footystats.org/img/teams/luton-town-fc.png'),
-('Bournemouth', 'Bournemouth', 1, 'https://cdn.footystats.org/img/teams/afc-bournemouth.png');
+('Arsenal', 'London', 1, 'https://github.com/Jsobrino98/ProyectoFinalAD/raw/main/codigo/SistemaTorneosCompeticions/src/main/resources/static/premier2425-escudos-main/arsenal.png'),
+('Manchester City', 'Manchester', 1, 'https://github.com/Jsobrino98/ProyectoFinalAD/raw/main/codigo/SistemaTorneosCompeticions/src/main/resources/static/premier2425-escudos-main/manchestercity.png'),
+('Manchester United', 'Manchester', 1, 'https://github.com/Jsobrino98/ProyectoFinalAD/raw/main/codigo/SistemaTorneosCompeticions/src/main/resources/static/premier2425-escudos-main/manchesterunited.png'),
+('Chelsea', 'London', 1, 'https://github.com/Jsobrino98/ProyectoFinalAD/raw/main/codigo/SistemaTorneosCompeticions/src/main/resources/static/premier2425-escudos-main/chelsea.png'),
+('Liverpool', 'Liverpool', 1, 'https://github.com/Jsobrino98/ProyectoFinalAD/raw/main/codigo/SistemaTorneosCompeticions/src/main/resources/static/premier2425-escudos-main/liverpool.png'),
+('Tottenham Hotspur', 'London', 1, 'https://github.com/Jsobrino98/ProyectoFinalAD/raw/main/codigo/SistemaTorneosCompeticions/src/main/resources/static/premier2425-escudos-main/tottenhamhotspur.png'),
+('Newcastle United', 'Newcastle', 1, 'https://github.com/Jsobrino98/ProyectoFinalAD/raw/main/codigo/SistemaTorneosCompeticions/src/main/resources/static/premier2425-escudos-main/newcastleunited.png'),
+('Aston Villa', 'Birmingham', 1, 'https://github.com/Jsobrino98/ProyectoFinalAD/raw/main/codigo/SistemaTorneosCompeticions/src/main/resources/static/premier2425-escudos-main/astonvilla.png'),
+('West Ham United', 'London', 1, 'https://github.com/Jsobrino98/ProyectoFinalAD/raw/main/codigo/SistemaTorneosCompeticions/src/main/resources/static/premier2425-escudos-main/westhamunited.png'),
+('Brighton & Hove Albion', 'Brighton', 1, 'https://github.com/Jsobrino98/ProyectoFinalAD/raw/main/codigo/SistemaTorneosCompeticions/src/main/resources/static/premier2425-escudos-main/brightonandhovealbion.png'),
+('Brentford', 'London', 1, 'https://github.com/Jsobrino98/ProyectoFinalAD/raw/main/codigo/SistemaTorneosCompeticions/src/main/resources/static/premier2425-escudos-main/brentford.png'),
+('Crystal Palace', 'London', 1, 'https://github.com/Jsobrino98/ProyectoFinalAD/raw/main/codigo/SistemaTorneosCompeticions/src/main/resources/static/premier2425-escudos-main/crystalpalace.png'),
+('Fulham', 'London', 1, 'https://github.com/Jsobrino98/ProyectoFinalAD/raw/main/codigo/SistemaTorneosCompeticions/src/main/resources/static/premier2425-escudos-main/fulham.png'),
+('Wolverhampton Wanderers', 'Wolverhampton', 1, 'https://github.com/Jsobrino98/ProyectoFinalAD/raw/main/codigo/SistemaTorneosCompeticions/src/main/resources/static/premier2425-escudos-main/wolverhamptonwanderers.png'),
+('Nottingham Forest', 'Nottingham', 1, 'https://github.com/Jsobrino98/ProyectoFinalAD/raw/main/codigo/SistemaTorneosCompeticions/src/main/resources/static/premier2425-escudos-main/nottinghamforest.png'),
+('Everton', 'Liverpool', 1, 'https://github.com/Jsobrino98/ProyectoFinalAD/raw/main/codigo/SistemaTorneosCompeticions/src/main/resources/static/premier2425-escudos-main/everton.png'),
+('Burnley', 'Burnley', 1, 'https://github.com/Jsobrino98/ProyectoFinalAD/raw/main/codigo/SistemaTorneosCompeticions/src/main/resources/static/premier2425-escudos-main/burnley.png'),
+('Sheffield United', 'Sheffield', 1, 'https://github.com/Jsobrino98/ProyectoFinalAD/raw/main/codigo/SistemaTorneosCompeticions/src/main/resources/static/premier2425-escudos-main/sheffieldunited.png'),
+('Luton Town', 'Luton', 1, 'https://github.com/Jsobrino98/ProyectoFinalAD/raw/main/codigo/SistemaTorneosCompeticions/src/main/resources/static/premier2425-escudos-main/lutontown.png'),
+('Bournemouth', 'Bournemouth', 1, 'https://github.com/Jsobrino98/ProyectoFinalAD/raw/main/codigo/SistemaTorneosCompeticions/src/main/resources/static/premier2425-escudos-main/afcbournemouth.png');
+
+
+
+select * from equipos;
+
+DELETE FROM equipos WHERE torneo_id = 1;
+
 
 
 
