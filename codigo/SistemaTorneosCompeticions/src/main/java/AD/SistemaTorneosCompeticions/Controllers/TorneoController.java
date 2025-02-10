55c7ccfb9d6c00b1a0d0837affb4299c9d52a7c3
Jorge Sobrino <159003886+Jsobrino98@users.noreply.github.com>
2025-02-10 08:47:07 +0100

    Creacion ResultadoController
    Creacion metodos CRUD ResultadoController

--- codigo/SistemaTorneosCompeticions/src/main/java/AD/SistemaTorneosCompeticions/Controllers/TorneoController.java
+++ codigo/SistemaTorneosCompeticions/src/main/java/AD/SistemaTorneosCompeticions/Controllers/TorneoController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/torneo") // Corregido: el endpoint de base de la URL
+@RequestMapping("api/torneo")
 public class TorneoController {
 
     @Autowired
