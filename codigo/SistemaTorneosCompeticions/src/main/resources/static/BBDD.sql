e0da0c8aacae60571ee58639536d7515c29e193b
Jorge Sobrino <159003886+Jsobrino98@users.noreply.github.com>
2025-02-12 13:47:30 +0100

    Datos Equipos LaLiga SerieA

--- codigo/SistemaTorneosCompeticions/src/main/resources/static/BBDD.sql
+++ codigo/SistemaTorneosCompeticions/src/main/resources/static/BBDD.sql
@@ -110,7 +110,53 @@ VALUES
 
 
 
+-- Equipos LaLiga --
 
+INSERT INTO equipos (id, nombre, ciudad, torneo_id, escudoURL) VALUES
+(21, 'Athletic Club', 'Bilbao', 7, 'https://assets.laliga.com/assets/2019/06/07/xsmall/athletic.png'),
+(22, 'Atlético de Madrid', 'Madrid', 7, 'https://d2bk74em32frqb.cloudfront.net/assets/2024/06/17/xsmall/cbc5c8cc8c3e8abd0e175c00ee53b723.png'),
+(23, 'CA Osasuna', 'Pamplona', 7, 'https://d2bk74em32frqb.cloudfront.net/assets/2019/06/07/xsmall/osasuna.png'),
+(24, 'CD Leganés', 'Leganés', 7, 'https://d2bk74em32frqb.cloudfront.net/assets/2019/06/07/xsmall/leganes.png'),
+(25, 'Deportivo Alavés', 'Vitoria-Gasteiz', 7, 'https://d2bk74em32frqb.cloudfront.net/assets/2020/09/01/xsmall/27002754a98bf535807fe49a24cc63ea.png'),
+(26, 'FC Barcelona', 'Barcelona', 7, 'https://assets.laliga.com/assets/2019/06/07/xsmall/barcelona.png'),
+(27, 'Getafe CF', 'Getafe', 7, 'https://assets.laliga.com/assets/2023/05/12/xsmall/dc59645c96bc2c9010341c16dd6d4bfa.png'),
+(28, 'Girona FC', 'Girona', 7, 'https://assets.laliga.com/assets/2022/06/22/xsmall/8f43addbb29e4a72f5e90b6edfe4728f.png'),
+(29, 'Rayo Vallecano', 'Madrid', 7, 'https://assets.laliga.com/assets/2023/04/27/xsmall/57d9950a8745ead226c04d37235c0786.png'),
+(30, 'RC Celta', 'Vigo', 7, 'https://d2bk74em32frqb.cloudfront.net/assets/2019/06/07/xsmall/celta.png'),
+(31, 'RCD Espanyol de Barcelona', 'Cornellà de Llobregat', 7, 'https://d2bk74em32frqb.cloudfront.net/assets/2019/06/07/xsmall/espanyol.png'),
+(32, 'RCD Mallorca', 'Palma de Mallorca', 7, 'https://assets.laliga.com/assets/2023/03/22/xsmall/013ae97735bc8e519dcf30f6826168ca.png'),
+(33, 'Real Betis', 'Sevilla', 7, 'https://assets.laliga.com/assets/2022/09/15/xsmall/e4a09419d3bd115b8f3dab73d480e146.png'),
+(34, 'Real Madrid', 'Madrid', 7, 'https://assets.laliga.com/assets/2019/06/07/xsmall/real-madrid.png'),
+(35, 'Real Sociedad', 'San Sebastián', 7, 'https://assets.laliga.com/assets/2019/06/07/xsmall/real-sociedad.png'),
+(36, 'Real Valladolid CF', 'Valladolid', 7, 'https://assets.laliga.com/assets/2024/06/17/xsmall/1467dcd5efb813a742d86f8eb39504a3.png'),
+(37, 'Sevilla FC', 'Sevilla', 7, 'https://assets.laliga.com/assets/2019/06/07/xsmall/sevilla.png'),
+(38, 'UD Las Palmas', 'Las Palmas de Gran Canaria', 7, 'https://assets.laliga.com/assets/2019/06/07/xsmall/las-palmas.png'),
+(39, 'Valencia CF', 'Valencia', 7, 'https://assets.laliga.com/assets/2019/06/07/xsmall/valencia.png'),
+(40, 'Villarreal CF', 'Villarreal', 7, 'https://assets.laliga.com/assets/2019/06/07/xsmall/villarreal.png');
+
+
+-- Equipos Serie A --
+INSERT INTO equipos (id, nombre, ciudad, torneo_id, escudoURL) VALUES
+(41, 'Nápoles', 'Nápoles', 5, 'https://www.legaseriea.it/img/loghi/NAPOLI.svg'),
+(42, 'Inter de Milán', 'Milán', 5, 'https://www.legaseriea.it/img/loghi/INTER.svg'),
+(43, 'Atalanta', 'Bérgamo', 5, 'https://www.legaseriea.it/img/loghi/ATALANTA.svg'),
+(44, 'Lazio', 'Roma', 5, 'https://www.legaseriea.it/img/loghi/LAZIO.svg'),
+(45, 'Juventus', 'Turín', 5, 'https://www.legaseriea.it/img/loghi/JUVENTUS.svg'),
+(46, 'Fiorentina', 'Florencia', 5, 'https://www.legaseriea.it/img/loghi/FIORENTINA.svg'),
+(47, 'AC Milan', 'Milán', 5, 'https://www.legaseriea.it/img/loghi/MILAN.svg'),
+(48, 'Bolonia', 'Bolonia', 5, 'https://www.legaseriea.it/img/loghi/BOLOGNA.svg'),
+(49, 'Roma', 'Roma', 5, 'https://www.legaseriea.it/img/loghi/ROMA.svg'),
+(50, 'Udinese', 'Údine', 5, 'https://www.legaseriea.it/img/loghi/UDINESE.svg'),
+(51, 'Torino', 'Turín', 5, 'https://www.legaseriea.it/img/loghi/TORINO.svg'),
+(52, 'Génova', 'Génova', 5, 'https://www.legaseriea.it/img/loghi/GENOA.svg'),
+(53, 'Cagliari', 'Cagliari', 5, 'https://www.legaseriea.it/img/loghi/CAGLIARI.svg'),
+(54, 'Lecce', 'Lecce', 5, 'https://www.legaseriea.it/img/loghi/LECCE.svg'),
+(55, 'Hellas Verona', 'Verona', 5, 'https://www.legaseriea.it/img/loghi/VERONA.svg'),
+(56, 'Como 1907', 'Como', 5, 'https://www.legaseriea.it/img/loghi/COMO.svg'),
+(57, 'Empoli', 'Empoli', 5, 'https://www.legaseriea.it/img/loghi/EMPOLI.svg'),
+(58, 'Parma', 'Parma', 5, 'https://www.legaseriea.it/img/loghi/PARMA.svg'),
+(59, 'Venezia', 'Venecia', 5, 'https://www.legaseriea.it/img/loghi/VENEZIA.svg'),
+(60, 'Monza', 'Monza', 5, 'https://www.legaseriea.it/img/loghi/MONZA.svg');
 
 
 
