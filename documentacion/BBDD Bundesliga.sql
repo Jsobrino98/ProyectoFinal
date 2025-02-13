a6b397a5ac06b524e2254fd97bcc44baf36f12ae
Jorge Sobrino <159003886+Jsobrino98@users.noreply.github.com>
2025-02-13 09:40:33 +0100

    Actualizar Scripst Equipos

---
+++ documentacion/BBDD Bundesliga.sql
@@ -0,0 +1,21 @@
+
+
+INSERT INTO equipos (id, nombre, ciudad, torneo_id, escudoURL) VALUES
+(79, 'FC Bayern München', 'Múnich', 4, 'https://upload.wikimedia.org/wikipedia/commons/1/1f/FC_Bayern_München_logo_(2017).svg'),
+(80, 'Bayer 04 Leverkusen', 'Leverkusen', 4, 'https://upload.wikimedia.org/wikipedia/en/5/59/Bayer_04_Leverkusen_logo.svg'),
+(81, 'Eintracht Frankfurt', 'Fráncfort', 4, 'https://upload.wikimedia.org/wikipedia/commons/0/04/Eintracht_Frankfurt_Logo.svg'),
+(82, 'VfB Stuttgart', 'Stuttgart', 4, 'https://upload.wikimedia.org/wikipedia/commons/3/38/VfB_Stuttgart_Logo.svg'),
+(83, 'RB Leipzig', 'Leipzig', 4, 'https://upload.wikimedia.org/wikipedia/en/0/04/RB_Leipzig_2014_logo.svg'),
+(84, '1. FSV Mainz 05', 'Maguncia', 4, 'https://upload.wikimedia.org/wikipedia/commons/9/9e/FSV_Mainz_05_Logo.svg'),
+(85, 'VfL Wolfsburg', 'Wolfsburgo', 4, 'https://upload.wikimedia.org/wikipedia/commons/f/f3/Logo-VfL-Wolfsburg.svg'),
+(86, 'SC Freiburg', 'Friburgo', 4, 'https://upload.wikimedia.org/wikipedia/en/8/81/SC_Freiburg_logo.svg'),
+(87, 'SV Werder Bremen', 'Bremen', 4, 'https://upload.wikimedia.org/wikipedia/commons/b/be/SV-Werder-Bremen-Logo.svg'),
+(88, 'Borussia Dortmund', 'Dortmund', 4, 'https://upload.wikimedia.org/wikipedia/commons/6/67/Borussia_Dortmund_logo.svg'),
+(89, 'Borussia Mönchengladbach', 'Mönchengladbach', 4, 'https://upload.wikimedia.org/wikipedia/commons/8/81/Borussia_Mönchengladbach_logo.svg'),
+(90, 'FC Augsburg', 'Augsburgo', 4, 'https://upload.wikimedia.org/wikipedia/en/b/b5/FC_Augsburg_logo.svg'),
+(91, '1. FC Union Berlin', 'Berlín', 4, 'https://upload.wikimedia.org/wikipedia/commons/e/e7/1.FC_Union_Berlin_Logo.svg'),
+(92, 'FC St. Pauli', 'Hamburgo', 4, 'https://upload.wikimedia.org/wikipedia/commons/1/1b/FC_St_Pauli_logo.svg'),
+(93, 'TSG 1899 Hoffenheim', 'Sinsheim', 4, 'https://upload.wikimedia.org/wikipedia/commons/1/12/TSG_1899_Hoffenheim_Logo.svg'),
+(94, '1. FC Heidenheim', 'Heidenheim', 4, 'https://upload.wikimedia.org/wikipedia/commons/0/0a/1._FC_Heidenheim_1846_Logo.svg'),
+(95, 'Holstein Kiel', 'Kiel', 4, 'https://upload.wikimedia.org/wikipedia/commons/3/3d/Holstein_Kiel_Logo.svg'),
+(96, 'VfL Bochum', 'Bochum', 4, 'https://upload.wikimedia.org/wikipedia/commons/4/4f/VfL_Bochum_logo.svg');
---
+++ documentacion/BBDD Ligue1.sql
@@ -0,0 +1,24 @@
+
+
+
+INSERT INTO equipos (id, nombre, ciudad, torneo_id, escudoURL) VALUES
+(61, 'Paris Saint-Germain', 'París', 6, 'https://upload.wikimedia.org/wikipedia/en/a/a7/Paris_Saint-Germain_F.C..svg'),
+(62, 'Olympique de Marseille', 'Marsella', 6, 'https://upload.wikimedia.org/wikipedia/en/6/6d/Olympique_Marseille_logo.svg'),
+(63, 'OGC Nice', 'Niza', 6, 'https://upload.wikimedia.org/wikipedia/en/8/8b/OGC_Nice_logo.svg'),
+(64, 'AS Monaco', 'Mónaco', 6, 'https://upload.wikimedia.org/wikipedia/en/6/6b/AS_Monaco_FC.svg'),
+(65, 'LOSC Lille', 'Lille', 6, 'https://upload.wikimedia.org/wikipedia/en/6/62/LOSC_Lille_M%C3%A9tropole.svg'),
+(66, 'Olympique Lyonnais', 'Lyon', 6, 'https://upload.wikimedia.org/wikipedia/en/c/c6/Olympique_Lyonnais.svg'),
+(67, 'RC Lens', 'Lens', 6, 'https://upload.wikimedia.org/wikipedia/en/f/f6/RC_Lens_logo.svg'),
+(68, 'Stade Brestois 29', 'Brest', 6, 'https://upload.wikimedia.org/wikipedia/en/9/9e/Stade_Brestois_29_logo.svg'),
+(69, 'RC Strasbourg Alsace', 'Estrasburgo', 6, 'https://upload.wikimedia.org/wikipedia/en/6/6e/Racing_Club_de_Strasbourg_logo.svg'),
+(70, 'Toulouse FC', 'Toulouse', 6, 'https://upload.wikimedia.org/wikipedia/en/7/7e/Toulouse_FC_logo.svg'),
+(71, 'AJ Auxerre', 'Auxerre', 6, 'https://upload.wikimedia.org/wikipedia/en/5/5b/Logo_AJ_Auxerre.svg'),
+(72, 'Stade Rennais FC', 'Rennes', 6, 'https://upload.wikimedia.org/wikipedia/en/7/7e/Stade_Rennais_FC.svg'),
+(73, 'Angers SCO', 'Angers', 6, 'https://upload.wikimedia.org/wikipedia/en/0/0b/Angers_SCO_logo.svg'),
+(74, 'Stade de Reims', 'Reims', 6, 'https://upload.wikimedia.org/wikipedia/en/7/7f/Stade_de_Reims_logo.svg'),
+(75, 'FC Nantes', 'Nantes', 6, 'https://upload.wikimedia.org/wikipedia/en/3/3b/FC_Nantes_logo.svg'),
+(76, 'AS Saint-Étienne', 'Saint-Étienne', 6, 'https://upload.wikimedia.org/wikipedia/en/5/5f/ASSE_logo.svg'),
+(77, 'Le Havre AC', 'Le Havre', 6, 'https://upload.wikimedia.org/wikipedia/en/5/5e/Le_Havre_AC_logo.svg'),
+(78, 'Montpellier HSC', 'Montpellier', 6, 'https://upload.wikimedia.org/wikipedia/en/3/3f/Montpellier_HSC_logo.svg');
+
+
