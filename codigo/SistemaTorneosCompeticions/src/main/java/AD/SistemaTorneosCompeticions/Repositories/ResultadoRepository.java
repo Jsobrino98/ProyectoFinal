4e417f9cb49dba06bf5c16e3e8481b55e700e662
Jorge Sobrino <159003886+Jsobrino98@users.noreply.github.com>
2025-02-13 09:44:57 +0100

    Actualizar Modelos

--- codigo/SistemaTorneosCompeticions/src/main/java/AD/SistemaTorneosCompeticions/Repositories/ResultadoRepository.java
+++
@@ -1,9 +0,0 @@
-package AD.SistemaTorneosCompeticions.Repositories;
-
-import AD.SistemaTorneosCompeticions.Models.Resultado;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ResultadoRepository extends JpaRepository<Resultado, Long> {
-}
