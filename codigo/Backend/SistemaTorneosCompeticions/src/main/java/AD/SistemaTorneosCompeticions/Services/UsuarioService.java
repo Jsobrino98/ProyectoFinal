121f747c4b915977555dd300a92f87e8c6c22e32
Jorge Sobrino <159003886+Jsobrino98@users.noreply.github.com>
2025-02-18 12:52:31 +0100

    Actualizar

---
+++ codigo/Backend/SistemaTorneosCompeticions/src/main/java/AD/SistemaTorneosCompeticions/Services/UsuarioService.java
@@ -0,0 +1,51 @@
+package AD.SistemaTorneosCompeticions.Services;
+
+import AD.SistemaTorneosCompeticions.Models.DTO.UsuarioDTO;
+import AD.SistemaTorneosCompeticions.Models.Usuario;
+import AD.SistemaTorneosCompeticions.Repositories.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+public class UsuarioService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+
+    public Usuario guardar(Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
+    // Método para listar todos los USUARIOS
+    public List<UsuarioDTO> listarUsuarios() {
+        List<Usuario> usuarios = usuarioRepository.findAll();
+        return usuarios.stream().map(usuario -> new UsuarioDTO(
+                usuario.getNombreUsuario(),
+                usuario.getNombreCompleto(),
+                usuario.getEmail()
+        )).collect(Collectors.toList());
+    }
+
+
+    public Usuario obtener(Long id){
+        return usuarioRepository.findById(id).orElse(null);
+    }
+
+
+    public void eliminar(Long id) {
+        usuarioRepository.deleteById(id);
+    }
+
+    public Usuario findByUsernameOrEmail(String usernameOrEmail) {
+        Optional<Usuario> usuario = usuarioRepository.findByNombreUsuarioOrEmail(usernameOrEmail, usernameOrEmail);
+        return usuario.orElse(null); // Devuelve null si no se encuentra
+    }
+
+    public Optional<Usuario> findByUsername(String username) {
+        return usuarioRepository.findByNombreUsuario(username);
+    }
+}
